1ef3dd7127a3287541f1f1a6f46a71183999db5c
bxplayer <bxplayer10@gmail.com>
2022-01-13 08:53:39 -0300

    add test modificacion

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,5 +1,6 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +13,7 @@ public class BasicApplicationTests {
 	@Test
 	public void contextLoads() {
 		String message = "default message cambio 12";
-		Assert.assertNotNull(message);
+		//Assert.assertNotNull(message);
 	}
 
 }
